335c79f15a5d286e18e65986c8c921e0202c2823
liushaofei <1652396752@qq.com>
2019-08-05 16:42:36 +0800

    条件过滤的布局

--- pages/air/flights.vue
+++ pages/air/flights.vue
@@ -4,7 +4,7 @@
       <!-- 顶部过滤列表 -->
       <div class="flights-content">
         <!-- 过滤条件 -->
-        <div></div>
+        <FlightsFilters/>
 
         <!-- 航班头部布局 -->
         <FlightsListHead />
@@ -36,6 +36,7 @@
 <script>
 import FlightsListHead from "@/components/air/flightsListHead.vue";
 import FlightsItem from "@/components/air/flightsItem.vue";
+import FlightsFilters from '@/components/air/flightsFilters'
 
 import moment from "moment";
 
@@ -48,13 +49,14 @@ export default {
       listData: [],
 
       pageIndex: 1,
-      pageSize: 2,
+      pageSize: 8,
       total: 0
     };
   },
   components: {
     FlightsListHead,
-    FlightsItem
+    FlightsItem,
+    FlightsFilters
   },
   methods: {
     // 设置数据库listData
